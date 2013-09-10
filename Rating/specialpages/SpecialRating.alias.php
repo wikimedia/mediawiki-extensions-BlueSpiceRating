49f7a58c618eca181e71e019f5cae6a4ad3dada5
pwirth <wirth@hallowelt.biz>
2013-09-10 14:07:29 +0200

    * updated to latest internal_develop (SVN) r10349 version

---
+++ Rating/specialpages/SpecialRating.alias.php
@@ -0,0 +1,17 @@
+<?php
+$specialPageAliases = array();
+
+/** English */
+$specialPageAliases['en'] = array(
+    'SpecialRating' => array( 'Ratings' ),
+);
+
+/** German (Deutsch) */
+$specialPageAliases['de'] = array(
+	'SpecialRating' => array( 'Bewertungen' ),
+);
+
+/** German (Deutsch Formal) */
+$specialPageAliases['de-formal'] = array(
+	'SpecialRating' => array( 'Bewertungen' ),
+);
