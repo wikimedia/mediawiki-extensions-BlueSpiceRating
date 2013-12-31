e32f2578c324966d3ed966bec65716ec1ecc9667
pwirth <wirth@hallowelt.biz>
2013-12-31 12:33:38 +0100

    * refactoring ptI * HW#2013122810000025 — [BlueForge: Update - BS2]: Rating (https://otrs.hallowelt.biz/otrs/index.pl?Action=AgentTicketZoom;TicketID=5711)

---
+++ Rating/resources/i18n/SpecialRating.de-formal.js
@@ -0,0 +1,25 @@
+/**
+ * language de-formal for Rating special page
+ *
+ * @author     Patric Wirth <wirth@hallowelt.biz>
+ * @version    1.0.0 beta
+ * @version    $Id: SpecialRating.de-formal.js 6636 2012-09-28 13:45:59Z pwirth $
+ * @package    Bluespice_Extensions
+ * @subpackage Rating
+ * @copyright  Copyright (C) 2011 Hallo Welt! - Medienwerkstatt GmbH, All rights reserved.
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU Public License v2 or later
+ * @filesource
+ */
+
+RatingManagerI18N = {
+	cbRatingTypeLabel: 'Typ',
+	cbRatingTypeEmptyText: 'Typ auswählen',
+    titleTitle: 'Titel',
+    titleRating: 'Bewertung',
+	titleVotes: 'Anzahl der Bewertungen',
+    ptbDisplayMsgText: 'Seiten {0} - {1} von {2}',
+    ptbEmptyMsgText  : 'Keine Bewertungen gefunden.',
+    ptbBeforePageText: 'Seite',
+    ptbAfterPageText : 'von {0}',
+    loading: 'Lade weitere Informationen...'
+}
