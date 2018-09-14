0e5f3467f1dba58aab0492717c1679eb8cbf8bfc
Dejan Savuljesku <d.savuljesku@gmail.com>
2018-09-14 15:47:17 +0200

    Remove default values for like namespaces

--- src/Hook/BSApiNamespaceStoreMakeData/AddData.php
+++ src/Hook/BSApiNamespaceStoreMakeData/AddData.php
@@ -5,7 +5,6 @@ namespace BlueSpice\Rating\Hook\BSApiNamespaceStoreMakeData;
 use BlueSpice\NamespaceManager\Hook\BSApiNamespaceStoreMakeData;
 
 class AddData extends BSApiNamespaceStoreMakeData {
-	protected $defaultLikeNamespaces = [ 0, 4, 6, 10, 14 ];
 
 	protected function doProcess() {
 		$enabledNamespaces = $this->getConfig()->get( 'RatingArticleEnabledNamespaces' );
@@ -18,10 +17,8 @@ class AddData extends BSApiNamespaceStoreMakeData {
 				'disabled' => $result['isTalkNS']
 			];
 
-
 			$result['recommendations'] = [
 				'value' => in_array( $result[ 'id' ], $enabledLikeNamespaces ),
-				'read_only' => in_array( $result[ 'id' ], $this->defaultLikeNamespaces ),
 				'disabled' => $result['isTalkNS']
 			];
 
