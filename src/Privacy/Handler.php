51f70c4a29bdc72e7fb18bd1fa4b1a5191cff651
Robert Vogel <robert.vogel@hallowelt.com>
2018-12-05 08:05:06 +0000

    Merge branch 'privacy' into 'master'
    Add BSPrivacy handler
    
    See merge request BlueSpice/mediawiki-extensions-bluespicerating!69
    
    (cherry picked from commit 6216874b57f1ac2daa51bda32c8434c3c441ddf5)
    
    fa0d7a82 Add BSPrivacy handler
    01285982 Add export data handler

---
+++ src/Privacy/Handler.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace BBlueSpice\Rating\Privacy;
+
+use BlueSpice\Privacy\IPrivacyHandler;
+
+class Handler implements IPrivacyHandler {
+	protected $db;
+
+	public function __construct( \Database $db ) {
+		$this->db = $db;
+	}
+
+	public function anonymize( $oldUsername, $newUsername ) {
+		$this->db->update(
+			'bs_rating',
+			[ 'rat_userip' => $newUsername ],
+			[ 'rat_userip' => $oldUsername ]
+		);
+		return \Status::newGood();
+	}
+
+	public function delete( \User $userToDelete, \User $deletedUser ) {
+		$this->anonymize( $userToDelete->getName(), $deletedUser->getName() );
+
+		$this->db->update(
+			'bs_rating',
+			[ 'rat_userid' => $deletedUser->getId() ],
+			[ 'rat_userid' => $userToDelete->getId() ]
+		);
+
+		return \Status::newGood();
+	}
+
+	public function exportData( array $types, $format, \User $user ) {
+		// Where is Rating used?
+		return \Status::newGood( [] );
+	}
+}
