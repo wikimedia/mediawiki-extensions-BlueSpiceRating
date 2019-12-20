c3a39ca8d0fae1187b5c58479cf3c3b3b354497e
Dejan Savuljesku <d.savuljesku@gmail.com>
2019-12-20 14:59:28 +0100

    Migrate settings from DB to nm-settings.php
    Old settings in bs_settings3 table were blocking whatever user sets
    in NamespaceManager
    
    [3.1.x]
    
    ERM15947
    ERM12724
    
    Change-Id: Ibf52ea40f258b89b5d8e6c3df07a555be4a29ac9

---
+++ maintenance/BSReviewMigrateSettingsFromDB.php
@@ -0,0 +1,138 @@
+<?php
+
+$IP = dirname( dirname( dirname( __DIR__ ) ) );
+
+require_once "$IP/maintenance/Maintenance.php";
+
+use BlueSpice\Services;
+
+class BSReviewMigrateSettingsFromDB extends LoggedUpdateMaintenance {
+
+	/**
+	 * @inheritDoc
+	 */
+	public function execute() {
+		$namespaceManager = Services::getInstance()->getBSExtensionFactory()->getExtension(
+			'BlueSpiceNamespaceManager'
+		);
+		if ( $namespaceManager === null ) {
+			$this->output( 'BlueSpiceNamespaceManager is not enabled' . PHP_EOL );
+		} else {
+			return parent::execute();
+		}
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	protected function doDBUpdates() {
+		$this->output( 'Migrating settings for BlueSpiceRating to nm-settings.php...' );
+		$ratingArticleEnabledNamespaces = $this->getValuesFor( 'RatingArticleEnabledNamespaces' );
+		$ratingArticleLikeEnabledNamespaces = $this->getValuesFor( 'RatingArticleLikeEnabledNamespaces' );
+
+		$userNamespaces = NamespaceManager::getUserNamespaces( true );
+		$this->buildDefinitions(
+			'rating',
+			$ratingArticleEnabledNamespaces,
+			$userNamespaces
+		);
+		$this->buildDefinitions(
+			'recommendations',
+			$ratingArticleLikeEnabledNamespaces,
+			$userNamespaces
+		);
+		$status = NamespaceManager::setUserNamespaces( $userNamespaces );
+		if ( !$status['success'] ) {
+			$this->output( 'failed:' . $status['message'] . PHP_EOL );
+			return;
+		}
+
+		if (
+			$this->deleteDBSettings( 'RatingArticleEnabledNamespaces' ) &&
+			$this->deleteDBSettings( 'RatingArticleLikeEnabledNamespaces' )
+		) {
+			$this->output( 'failed removing settings from database' . PHP_EOL );
+			return;
+		}
+
+		$this->output( 'done' . PHP_EOL );
+	}
+
+	/**
+	 * @param string $prop
+	 * @return bool
+	 */
+	protected function deleteDBSettings( $prop ) {
+		$db = $this->getDB( DB_MASTER );
+		$db->delete(
+			'bs_settings3',
+			[ 's_name' => $prop ],
+			__METHOD__
+		);
+
+		if ( $db->affectedRows() === 0 ) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * @param string $varName
+	 * @param array $namespaces
+	 * @param array &$userNamespaces
+	 * @return array
+	 */
+	protected function buildDefinitions( $varName, $namespaces, &$userNamespaces ) {
+		$aliases = $GLOBALS['wgNamespaceAliases'];
+		$defs = [];
+		foreach ( $namespaces as $id ) {
+			if ( !isset( $userNamespaces[$id] ) ) {
+				$userNamespaces[$id] = [
+					'name' => BsNamespaceHelper::getNamespaceName( $id ),
+					'alias' => array_search( $id, $aliases ) ?: ''
+				];
+			}
+			$userNamespaces[$id][$varName] = true;
+		}
+
+		return $defs;
+	}
+
+	/**
+	 * @param string $prop
+	 * @return array
+	 */
+	protected function getValuesFor( $prop ) {
+		$res = $this->getDB( DB_REPLICA )->selectRow(
+			'bs_settings3',
+			[ 's_value' ],
+			[ 's_name' => $prop ],
+			__METHOD__
+		);
+
+		if ( !$res ) {
+			return [];
+		}
+
+		$val = $res->s_value;
+		$parsed = eval( "return $val;" );
+		if ( is_array( $parsed ) ) {
+			return $parsed;
+		}
+
+		return [];
+	}
+
+	/**
+	 * Get the update key name to go in the update log table
+	 * @return string
+	 */
+	protected function getUpdateKey() {
+		return 'bs_review_settings_remove';
+	}
+
+}
+
+$maintClass = BSReviewMigrateSettingsFromDB::class;
+require_once RUN_MAINTENANCE_IF_MAIN;
