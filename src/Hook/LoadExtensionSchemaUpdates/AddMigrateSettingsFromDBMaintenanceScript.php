7f11cb651b8cc5d6506b6dd74c6b7b455f43be60
Dejan Savuljesku <d.savuljesku@gmail.com>
2019-12-20 14:59:28 +0100

    Migrate settings from DB to nm-settings.php
    Old settings in bs_settings3 table were blocking whatever user sets
    in NamespaceManager
    
    [3.1.x]
    
    ERM15947
    ERM12724
    
    Change-Id: Ibf52ea40f258b89b5d8e6c3df07a555be4a29ac9

---
+++ src/Hook/LoadExtensionSchemaUpdates/AddMigrateSettingsFromDBMaintenanceScript.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace BlueSpice\Rating\Hook\LoadExtensionSchemaUpdates;
+
+use BlueSpice\Hook\LoadExtensionSchemaUpdates;
+
+class AddMigrateSettingsFromDBMaintenanceScript extends LoadExtensionSchemaUpdates {
+	protected function doProcess() {
+		$this->updater->addPostDatabaseUpdateMaintenance(
+			'BSReviewMigrateSettingsFromDB'
+		);
+		return true;
+	}
+
+}
