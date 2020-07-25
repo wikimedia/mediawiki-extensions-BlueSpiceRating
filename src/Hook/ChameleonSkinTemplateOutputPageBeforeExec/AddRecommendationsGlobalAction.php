2d848ac8e80cd60ef4b3bcfe0118dd2c7a44ae9e
pwirth <wirth@hallowelt.biz>
2020-07-25 16:06:28 +0200

    Fix binding on deprecated hook
    T254300
    => Requires: Ief79d2d27a9c2754fc0f5513eb3a000da3ab3473
    
    [3.3]
    
    Change-Id: I190c83e9446c698799d8e9812253ae1f155ae549

--- src/Hook/ChameleonSkinTemplateOutputPageBeforeExec/AddRecommendationsGlobalAction.php
+++ src/Hook/ChameleonSkinTemplateOutputPageBeforeExec/AddRecommendationsGlobalAction.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace BlueSpice\Rating\Hook\SkinTemplateOutputPageBeforeExec;
+namespace BlueSpice\Rating\Hook\ChameleonSkinTemplateOutputPageBeforeExec;
 
-use BlueSpice\Hook\SkinTemplateOutputPageBeforeExec;
+use BlueSpice\Calumma\Hook\ChameleonSkinTemplateOutputPageBeforeExec;
 use BlueSpice\SkinData;
 
-class AddRecommendationsGlobalAction extends SkinTemplateOutputPageBeforeExec {
+class AddRecommendationsGlobalAction extends ChameleonSkinTemplateOutputPageBeforeExec {
 
 	/**
 	 *
