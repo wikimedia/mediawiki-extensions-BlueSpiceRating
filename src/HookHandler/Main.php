598f204b075428865185b10d61468ee75b40249a
miriam schlindwein <schlindwein@hallowelt.com>
2021-08-11 14:00:16 +0200

    Add extension to SkinSlo GlobalActionsTool
    [4.x]
    
    Change-Id: Ieecb12bf83e3791f6fa020c81579a2c96e39390e

---
+++ src/HookHandler/Main.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace BlueSpice\Rating\HookHandler;
+
+use BlueSpice\Rating\GlobalActionsToolRating;
+use BlueSpice\Rating\GlobalActionsToolRecommendations;
+use MWStake\MediaWiki\Component\CommonUserInterface\Hook\MWStakeCommonUIRegisterSkinSlotComponents;
+
+class Main implements MWStakeCommonUIRegisterSkinSlotComponents {
+
+	/**
+	 * @inheritDoc
+	 */
+	public function onMWStakeCommonUIRegisterSkinSlotComponents( $registry ): void {
+		$registry->register(
+			'GlobalActionsTools',
+			[
+				'special-bluespice-rating' => [
+					'factory' => function () {
+						return new GlobalActionsToolRating();
+					}
+				],
+				'special-bluespice-recommendations' => [
+					'factory' => function () {
+						return new GlobalActionsToolRecommendations();
+					}
+				]
+			]
+
+		);
+	}
+}
