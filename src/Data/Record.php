7126382367d5c50551a9bc6979cfa285d0d903f3
Alexander Vorwerk <alexander.vorwerk@stud.uni-goettingen.de>
2021-10-15 13:40:22 +0200

    Add visibility to constants
    We support PHP 7.3.19 or later, so we should declare the visibility of
    constants.
    
    Bug: T237840
    Change-Id: If52206cbad188da3ce4d88c56fadead89495a9f9

--- src/Data/Record.php
+++ src/Data/Record.php
@@ -3,16 +3,16 @@
 namespace BlueSpice\Rating\Data;
 
 class Record extends \BlueSpice\Data\Record {
-	const ID = 'rat_id';
-	const REFTYPE = 'rat_reftype';
-	const REF = 'rat_ref';
-	const USERID = 'rat_userid';
-	const USERIP = 'rat_userip';
-	const VALUE = 'rat_value';
-	const CREATED = 'rat_created';
-	const TOUCHED = 'rat_touched';
-	const SUBTYPE = 'rat_subtype';
-	const CONTEXT = 'rat_context';
+	public const ID = 'rat_id';
+	public const REFTYPE = 'rat_reftype';
+	public const REF = 'rat_ref';
+	public const USERID = 'rat_userid';
+	public const USERIP = 'rat_userip';
+	public const VALUE = 'rat_value';
+	public const CREATED = 'rat_created';
+	public const TOUCHED = 'rat_touched';
+	public const SUBTYPE = 'rat_subtype';
+	public const CONTEXT = 'rat_context';
 
 	public function __clone() {
 		$this->dataSet = clone $this->dataSet;
