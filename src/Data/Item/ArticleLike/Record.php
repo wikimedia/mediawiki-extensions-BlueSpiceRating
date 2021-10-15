5c78e2403f4ec0c85e7a0cfce7850076efb2f88e
Alexander Vorwerk <alexander.vorwerk@stud.uni-goettingen.de>
2021-10-15 13:40:22 +0200

    Add visibility to constants
    We support PHP 7.3.19 or later, so we should declare the visibility of
    constants.
    
    Bug: T237840
    Change-Id: If52206cbad188da3ce4d88c56fadead89495a9f9

--- src/Data/Item/ArticleLike/Record.php
+++ src/Data/Item/ArticleLike/Record.php
@@ -3,8 +3,8 @@
 namespace BlueSpice\Rating\Data\Item\ArticleLike;
 
 class Record extends \BlueSpice\Rating\Data\Item\Record {
-	const PAGETITLE = 'page_title';
-	const PAGENAMESPACE = 'page_namespace';
-	const PAGENAMESPACETEXT = 'page_namespace_text';
-	const TOTALCOUNT = 'totalcount';
+	public const PAGETITLE = 'page_title';
+	public const PAGENAMESPACE = 'page_namespace';
+	public const PAGENAMESPACETEXT = 'page_namespace_text';
+	public const TOTALCOUNT = 'totalcount';
 }
