27a722e8a173baa43f8263c6847601fec8cc1173
pavloyashchenko <pyashchenko@hallowelt.com>
2022-08-04 16:58:32 +0200

    Create AbstractSchema for sql table
    Create Json file; Generate 3 files(sqlite, postgres, mysql); Change path in addExtensionTable() to generated file.
    
    ERM29227
    
    [4.2.x]
    
    Change-Id: I782fe5a42d37f1378b929ce923bf448460203053
    (cherry picked from commit e4ca72bfa16fc18ad38e8a55cc4fd9ee2910a70c)

--- src/Hook/LoadExtensionSchemaUpdates/AddRatingDatabase.php
+++ src/Hook/LoadExtensionSchemaUpdates/AddRatingDatabase.php
@@ -10,22 +10,26 @@ class AddRatingDatabase extends LoadExtensionSchemaUpdates {
 	 * @return bool
 	 */
 	protected function doProcess() {
-		$dir = $this->getExtensionPath() . '/maintenance/db';
+		$dbType = $this->updater->getDB()->getType();
+		$dir = $this->getExtensionPath();
 
 		$this->updater->addExtensionTable(
 			'bs_rating',
-			"$dir/rating.sql"
+			"$dir/maintenance/db/sql/$dbType/rating-generated.sql"
 		);
-		$this->updater->addExtensionField(
-			'bs_rating',
-			'rat_subtype',
-			"$dir/bs_rating.newfield.rat_subtype.sql"
-		);
-		$this->updater->addExtensionField(
-			'bs_rating',
-			'rat_context',
-			"$dir/bs_rating.newfield.rat_context.sql"
-		);
+
+		if ( $dbType == 'mysql' ) {
+			$this->updater->addExtensionField(
+				'bs_rating',
+				'rat_subtype',
+				"$dir/maintenance/db/bs_rating.newfield.rat_subtype.sql"
+			);
+			$this->updater->addExtensionField(
+				'bs_rating',
+				'rat_context',
+				"$dir/maintenance/db/bs_rating.newfield.rat_context.sql"
+			);
+		}
 		return true;
 	}
 
