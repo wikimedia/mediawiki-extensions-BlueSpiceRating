b73835fe57887a172b5abd3fc3de8aa37f848925
Hamish Slater <hamishslater@hotmail.com>
2022-09-13 09:56:23 +0200

    Fix protected '$services' in base class
    ERM29749
    
    [4.x]
    
    Change-Id: I1dd72d7d9139fa9ec17d0d258fa04f9fabbd2599

--- maintenance/BSRatingMigrateRatedComments.php
+++ maintenance/BSRatingMigrateRatedComments.php
@@ -11,7 +11,7 @@ use MediaWiki\MediaWikiServices;
 class BSRatingMigrateRatedComments extends LoggedUpdateMaintenance {
 
 	/** @var MediaWikiServices */
-	private $services = null;
+	protected $services = null;
 
 	public function __construct() {
 		parent::__construct();
