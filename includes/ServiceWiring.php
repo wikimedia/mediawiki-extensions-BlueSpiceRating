52787ea64a12b0f4750eba0a9de8dda4716f76d6
Dreamy Jazz <dreamyjazzwikipedia@gmail.com>
2022-12-23 02:35:21 +0000

    Test ServiceWiring.php
    Test ServiceWiring.php using tests copied from CentralAuth. Because
    phpunit does not support marking a file as covered, the ServiceWiring
    file is ignored for code coverage as the tests fully cover the file.
    
    Change-Id: Ia3e6f8aa343e63702d214a42bcd30021fde72169

--- includes/ServiceWiring.php
+++ includes/ServiceWiring.php
@@ -2,6 +2,11 @@
 
 use MediaWiki\MediaWikiServices;
 
+// PHP unit does not understand code coverage for this file
+// as the @covers annotation cannot cover a specific file
+// This is fully tested in ServiceWiringTest.php
+// @codeCoverageIgnoreStart
+
 return [
 
 	'BSRatingRegistry' => static function ( MediaWikiServices $services ) {
@@ -41,3 +46,5 @@ return [
 		);
 	},
 ];
+
+// @codeCoverageIgnoreEnd
