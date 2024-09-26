8a42edb0e6b3a535405edea3e7ed8b838f7c95b1
hslater <hamish.slater@hallowelt.com>
2024-09-27 00:03:10 +1200

    WCAG Recommend
    disable if !usercanmodify
    change label to recommended if recommended
    add label for screenreader
    
    ERM38758
    
    [REL1_39, REL1_39-4.5.x, master]
    
    Change-Id: Ic57025280671aed60c3d2d5a798c8f9f741019d3
    (cherry picked from commit 36f8f53a5a717b3ad389751eb720a2a2352b3e32)

--- resources/bluespice.ratingItemArticleLike.js
+++ resources/bluespice.ratingItemArticleLike.js
@@ -1,120 +1,133 @@
-/**
- * @author     Patric Wirth
- * @package    BluespiceRating
- * @subpackage Rating
- * @copyright  Copyright (C) 2017 Hallo Welt! GmbH, All rights reserved.
- * @license    http://www.gnu.org/copyleft/gpl.html GPL-3.0-only
- */
-
-bs.rating.ItemArticleLike = function( $el, type, data ) {
+bs.rating.ItemArticleLike = function ( $el, type, data ) {
 	bs.rating.Item.call( this, $el, type, data );
-	this.makeVoteButton();
-	this.makeNumVotes();
-	this.makeUserVoted();
+
+	this.$voteButton = this.makeVoteButton();
 
 	this.getEl().append( this.$voteButton );
-	this.getEl().append( this.$numVotes );
-	this.getEl().append( this.$userVoted );
+	this.getEl().append( this.makeUserVoted() );
 
+	if ( !this.data.get( 'usercanmodify', false ) ) {
+		this.$voteButton.prop( 'disabled', true );
+		this.$voteButton.attr( 'aria-disabled', 'true' );
+	}
+
+	this.updateVoteButtonAccessibility();
 	if ( this.userVoted() ) {
 		this.$voteButton.addClass( 'uservoted' );
-		this.$voteButton.attr( 'aria-pressed', 'true' );
-		this.$numVotes.addClass( 'uservoted' );
+		this.$label.addClass( 'uservoted' );
 		this.$userVoted.show();
-	} else {
-		this.$voteButton.attr( 'aria-pressed', 'false' );
+	}
+
+	if ( this.getVoteCount() > 0 ) {
+		this.$voteIcon.addClass( 'recommended' );
 	}
 
 	this.$voteButton.on( 'keydown', ( e ) => {
 		if ( e.key === 'Enter' || e.key === ' ' ) {
 			e.preventDefault();
 			e.stopPropagation();
-			this.$voteButton.click();
+			this.$voteButton.trigger( 'click' );
 		}
-	});
+	} );
 
-	$( this.getEl() ).on(
-		"click",
-		".bs-rating-articlelike-button, .bs-rating-articlelike-numvotes",
-		() => {
-		if( !this.data.get( 'usercanmodify', false ) ) {
+	this.$voteButton.on( 'click', () => {
+		if ( !this.data.get( 'usercanmodify', false ) ) {
 			return false;
 		}
 		this.addLoadingMask();
 		this.vote( this.userVoted() ? false : 1 ).done( ( result ) => {
-			if( result.success === true ) {
+			if ( result.success === true ) {
 				this.reset( result.payload.data );
 				this.removeLoadingMask();
 			}
-		});
+		} );
 		return false;
-	});
+	} );
 };
 OO.inheritClass( bs.rating.ItemArticleLike, bs.rating.Item );
 bs.rating.register(
 	'articlelike',
-	"\\BlueSpice\\Rating\\RatingItem\\ArticleLike",
+	'\\BlueSpice\\Rating\\RatingItem\\ArticleLike',
 	bs.rating.ItemArticleLike
 );
 
-bs.rating.ItemArticleLike.prototype.getData = function() {
-	var data = bs.rating.ItemArticleLike.super.prototype.getData.apply( this );
+bs.rating.ItemArticleLike.prototype.getData = function () {
+	const data = bs.rating.ItemArticleLike.super.prototype.getData.apply( this );
 	data.articleid = mw.config.get( 'wgArticleId', 0 );
 	return data;
 };
 
-bs.rating.ItemArticleLike.prototype.reset = function( data ) {
-	bs.rating.ItemArticleLike.super.prototype.reset.apply( this, [data] );
-	this.getEl().find('.bs-rating-articlelike-numvotes').replaceWith(
-		this.makeNumVotes()
+bs.rating.ItemArticleLike.prototype.reset = function ( data ) {
+	bs.rating.ItemArticleLike.super.prototype.reset.apply( this, [ data ] );
+	this.getEl().find( '.bs-rating-articlelike-label' ).replaceWith(
+		this.makeLabel()
 	);
-	if( this.userVoted() ) {
-		this.$voteButton.addClass( 'uservoted' );
-		this.$voteButton.attr( 'aria-pressed', 'true' );
-		this.$voteButton.attr( 'aria-label', mw.message( 'bs-rating-articlelike-uratingtextservoted', this.getVoteCount() ).plain() );
-		this.$numVotes.addClass( 'uservoted' );
+	this.updateVoteButtonAccessibility();
+	if ( this.userVoted() ) {
+		this.$voteIcon.addClass( 'recommended' );
 		this.$userVoted.show();
 	} else {
-		this.$voteButton.removeClass( 'uservoted' );
-		this.$voteButton.attr( 'aria-pressed', 'false' );
-		this.$voteButton.attr( 'aria-label', mw.message( 'bs-rating-articlelike-ratingtext', this.getVoteCount() ).plain() );
-		this.$numVotes.removeClass( 'uservoted' );
 		this.$userVoted.hide();
+		if ( this.getVoteCount() === 0 ) {
+			this.$voteIcon.removeClass( 'recommended' );
+		}
 	}
 };
 
-bs.rating.ItemArticleLike.prototype.makeVoteButton = function( data ) {
-	this.$voteButton = $('<button></button>').attr( {
-		'class': 'bs-rating-articlelike-button',
-		'role': 'button',
+bs.rating.ItemArticleLike.prototype.updateVoteButtonAccessibility = function () {
+	this.$voteButton.attr( 'title', mw.message( 'bs-rating-articlelike-ratingtext-title', this.getVoteCount() ).text() );
+	if ( this.userVoted() ) {
+		this.$voteButton.attr( 'aria-pressed', 'true' );
+		this.$voteButton.attr( 'aria-label', mw.message( 'bs-rating-articlelike-uratingtextservoted', this.getVoteCount() ).text() );
+	} else {
+		this.$voteButton.attr( 'aria-pressed', 'false' );
+		this.$voteButton.attr( 'aria-label', mw.message( 'bs-rating-articlelike-ratingtext', this.getVoteCount() ).text() );
+	}
+};
+
+bs.rating.ItemArticleLike.prototype.makeVoteButton = function () {
+	this.$voteButton = $( '<button>' ).attr( {
+		class: 'bs-rating-articlelike-button',
+		title: mw.message( 'bs-rating-articlelike-ratingtext-title', this.getVoteCount() ).text(),
+		role: 'button',
 		'aria-pressed': 'false',
-		'aria-label': mw.message(
-			this.userVoted()
-			? 'bs-rating-articlelike-uratingtextservoted'
-			: 'bs-rating-articlelike-ratingtext', this.getVoteCount()
-		).plain(),
-		'tabindex': '0'
+		tabindex: '0'
 	} );
+
+	this.$voteButton.append( this.makeVoteIcon() );
+	this.$voteButton.append( this.makeLabel() );
 
 	return this.$voteButton;
 };
 
-bs.rating.ItemArticleLike.prototype.makeNumVotes = function( data ) {
-	this.$numVotes = $(
-		'<span class="bs-rating-articlelike-numvotes">'
-		+ mw.message(
-			this.userVoted()
-				? 'bs-rating-articlelike-uratingtextservoted'
-				: 'bs-rating-articlelike-ratingtext',
-			this.getVoteCount()
-		).parse()
-		+ '</span>'
-	);
-	return this.$numVotes;
+bs.rating.ItemArticleLike.prototype.makeVoteIcon = function () {
+	this.$voteIcon = $( '<span>' ).attr( {
+		class: 'bs-rating-articlelike-icon'
+	} );
+
+	return this.$voteIcon;
 };
 
-bs.rating.ItemArticleLike.prototype.makeUserVoted = function( data ) {
-	this.$userVoted = $(
-		'<span class="bs-rating-articlelike-uservoted"></span>'
-	).hide();
+bs.rating.ItemArticleLike.prototype.makeLabel = function () {
+	this.$label = $( '<span>' ).attr( {
+		class: 'bs-rating-articlelike-label'
+	} ).html(
+		mw.message(
+			this.getVoteCount() > 0 ?
+				'bs-rating-articlelike-uratingtextservoted' :
+				'bs-rating-articlelike-ratingtext',
+			this.getVoteCount()
+		).parse()
+	);
+
+	return this.$label;
+};
+
+bs.rating.ItemArticleLike.prototype.makeUserVoted = function () {
+	this.$userVoted = $( '<span>' ).attr( {
+		class: 'bs-rating-articlelike-uservoted',
+		title: mw.message( 'bs-rating-articlelike-uservoted-title' ).text()
+	} ).hide();
+
+	return this.$userVoted;
 };
