705049f2e9bc8bd6c2e1a3da1825b303074a2866
hslater <hamish.slater@hallowelt.com>
2025-05-06 15:58:24 +1200

    eslint
    ERM20742
    
    [REL1_43, REL1_43-5.0.x, master]
    
    Change-Id: I8a83a04e8c57cdeedc12da086a6c21abf5fe8376
    (cherry picked from commit 5123338a553059ef3c0634c162a5578df4c8a539)

--- resources/bluespice.rating.js
+++ resources/bluespice.rating.js
@@ -1,91 +1,82 @@
-/**
- * Js for Rating extension
- *
- * @author     Patric Wirth
- * @package    BlueSpiceRating
- * @subpackage Rating
- * @copyright  Copyright (C) 2016 Hallo Welt! GmbH, All rights reserved.
- * @license    http://www.gnu.org/copyleft/gpl.html GPL-3.0-only
- */
-
-OO = window.OO; //To make OOJS available within the RL modules
+// To make OOJS available within the RL modules
+OO = window.OO; // eslint-disable-line no-global-assign, no-implicit-globals
 window.bs = window.bs || {};
 window.bs.rating = window.bs.rating || {};
 bs.rating.factory = new OO.Factory();
 bs.rating.types = {};
 bs.rating.itemStore = {};
-bs.rating.register = function( sType, sStaticName, oClass ) {
+bs.rating.register = function ( sType, sStaticName, oClass ) {
 	oClass.static.name = sStaticName;
 	bs.rating.factory.register( oClass );
-	bs.rating.types[sType] = sStaticName;
+	bs.rating.types[ sType ] = sStaticName;
 };
 bs.rating.uuid = 0;
-bs.rating.generateUniqueId = function() {
-	return "bsr-ui-" + ( ++bs.rating.uuid );
+bs.rating.generateUniqueId = function () {
+	return 'bsr-ui-' + ( ++bs.rating.uuid );
 };
-var config = require( './config.json' );
+const config = require( './config.json' );
 bs.rating.config = config.ratingConfig;
-bs.rating.getUiID = function( $el ) {
-	if( $el.attr('id') && $el.attr('id').length > 0 ) {
-		return $el.attr('id');
+bs.rating.getUiID = function ( $el ) {
+	if ( $el.attr( 'id' ) && $el.attr( 'id' ).length > 0 ) {
+		return $el.attr( 'id' );
 	}
 	return false;
 };
-bs.rating.getFromStore = function( uiID ) {
-	if( !uiID || !bs.rating.itemStore[uiID] ) {
+bs.rating.getFromStore = function ( uiID ) {
+	if ( !uiID || !bs.rating.itemStore[ uiID ] ) {
 		return null;
 	}
-	return bs.rating.itemStore[uiID];
+	return bs.rating.itemStore[ uiID ];
 };
 
-bs.rating.newFromEl = function( $el ) {
-	if( !$el || $el.length < 1 ) {
+bs.rating.newFromEl = function ( $el ) {
+	if ( !$el || $el.length < 1 ) {
 		return null;
 	}
-	var uiID = bs.rating.getUiID( $el );
-	var item = bs.rating.getFromStore( uiID );
-	if( item ) {
+	const uiID = bs.rating.getUiID( $el );
+	const item = bs.rating.getFromStore( uiID );
+	if ( item ) {
 		return item;
 	}
 	return bs.rating.createFromEl( $el );
 };
-bs.rating.createFromEl = function( $el ) {
-	if( !$el || $el.length < 1 ) {
+bs.rating.createFromEl = function ( $el ) {
+	if ( !$el || $el.length < 1 ) {
 		return null;
 	}
-	var type = $el.attr( 'data-type' );
-	if( !type || type === '' || typeof type === 'undefined' ) {
+	let type = $el.attr( 'data-type' );
+	if ( !type || type === '' || typeof type === 'undefined' ) {
 		type = 'base';
 	}
 
-	if( typeof bs.rating.types[type] === "undefined" ) {
-		throw "Unregistered type: " + type;
+	if ( typeof bs.rating.types[ type ] === 'undefined' ) {
+		throw new Error( `Unregistered type: ${ type }` );
 	}
 
-	if( typeof $el.attr('data-item') === "undefined" ) {
-		throw "The fild 'data-item' is missing.";
+	if ( typeof $el.attr( 'data-item' ) === 'undefined' ) {
+		throw new Error( "The field 'data-item' is missing." );
 	}
 
-	var data = JSON.parse( $el.attr('data-item') );
+	const data = JSON.parse( $el.attr( 'data-item' ) );
 
-	var ratingitem = bs.rating.factory.create(
-		bs.rating.types[type],
+	const ratingitem = bs.rating.factory.create(
+		bs.rating.types[ type ],
 		$el,
 		type,
 		data
 	);
-	bs.rating.itemStore[ratingitem.makeUiID()] = ratingitem;
+	bs.rating.itemStore[ ratingitem.makeUiID() ] = ratingitem;
 };
-bs.rating.init = function(){
-	$(".bs-rating").each( function() {
-		if( bs.rating.getUiID( $(this) ) ) {
-			//already exists
+bs.rating.init = function () {
+	$( '.bs-rating' ).each( function () {
+		if ( bs.rating.getUiID( $( this ) ) ) {
+			// already exists
 			return null;
 		}
-		bs.rating.createFromEl( $(this) );
-	});
+		bs.rating.createFromEl( $( this ) );
+	} );
 };
 
-mw.loader.using( config.ratingModules ).done( function() {
+mw.loader.using( config.ratingModules ).done( () => {
 	bs.rating.init();
-});
+} );
